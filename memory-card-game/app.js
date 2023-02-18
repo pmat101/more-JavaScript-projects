395ad399b8588bc0dd27efea4f2e95203149ab24
Pranav Mathur <pranavmathur2.0@gmail.com>
2023-02-18 23:50:27 +0530

    basic working of app done, score keeping remains

--- memory-card-game/app.js
+++ memory-card-game/app.js
@@ -8,24 +8,55 @@ for (let i = 0; i < pokedex.length * 2; i++) {
   jumble.push(pokedex2[random]); // Get Pokemon pair in random positions
   pokedex2.splice(random, 1);
 }
-
+/*
+  ek card khola
+  dusra card khola
+  match kiya
+  if matched khole rakho (dont remove animation class)
+  else revert
+*/
 let current1, current2;
 
 const turn = (id) => {
-  if (current1 == undefined) assign(current1, id);
-  else if (current2 == undefined) assign(current2, id);
-  else revert(current1, current2);
+  if (current1 == undefined) current1 = assign(id);
+  else if (current2 == undefined && id != current1.getAttribute("id")) {
+    current2 = assign(id);
+    match();
+    setTimeout(function () {
+      current1 = undefined;
+      current2 = undefined;
+    }, 6000);
+  }
 };
 
-const assign = (param, id) => {
-  param = document.getElementById(id);
-  param.setAttribute("id", "animate");
+const assign = (id) => {
+  let param = document.getElementById(id);
+  param.setAttribute("class", "animate");
   setTimeout(function () {
     param.setAttribute("src", `./img/${jumble[id]}.png`);
   }, 1000);
   setTimeout(function () {
-    param.removeAttribute("id", "animate");
+    param.removeAttribute("class", "animate");
   }, 2000);
+  return param;
 };
 
-const revert = (param, id) => {};
+const match = () => {
+  let id1 = current1.getAttribute("id");
+  let id2 = current2.getAttribute("id");
+  if (jumble[id1] == jumble[id2]) console.log("its a match");
+  else {
+    setTimeout(function () {
+      current1.setAttribute("class", "animate");
+      current2.setAttribute("class", "animate");
+    }, 4000);
+    setTimeout(function () {
+      current1.setAttribute("src", "./img/gettyimages-157772536-2048x2048.jpg");
+      current2.setAttribute("src", "./img/gettyimages-157772536-2048x2048.jpg");
+    }, 5000);
+    setTimeout(function () {
+      current1.removeAttribute("class", "animate");
+      current2.removeAttribute("class", "animate");
+    }, 6000);
+  }
+};
